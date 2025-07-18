6d58ac8f96f3766da48d0a176853cfc3b624eeda
Tulasi Reddy <tulasireddy.kagithala@karkinos.in>
2025-07-18 23:32:42 +0530

    add: Update the Threads list view from bottom to top.

--- app/src/main/java/com/whatsapp_notes/ui/screens/notes_view_screen/NoteViewScreen.kt
+++ app/src/main/java/com/whatsapp_notes/ui/screens/notes_view_screen/NoteViewScreen.kt
@@ -153,7 +153,7 @@ fun NoteViewScreen(
                 .fillMaxSize()
                 .padding(paddingValues)
                 .padding(start = 16.dp, end = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Bottom),
             contentPadding = PaddingValues(top = 16.dp, bottom = 16.dp)
         ) {
             items(threads) { threadUiState ->
